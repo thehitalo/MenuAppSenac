b95d256cb7f816e53a821f748035c0f8be9455fc
hitalo.pcarvalho <carvhitalo82@gmail.com>
2019-04-29 12:23:39 -0300

    Initial commit

---
+++ app/src/main/java/sp/senac/br/menuappsenac/Layout_Activity.java
@@ -0,0 +1,13 @@
+package sp.senac.br.menuappsenac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Layout_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_activity);
+    }
+}
